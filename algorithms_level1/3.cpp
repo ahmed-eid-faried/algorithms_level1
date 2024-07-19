c50b7ce8d9e0bab6be07b463a1780c9546303333
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-19 16:49:05 +0300

    ChechPersonIsHired

--- algorithms_level1/3.cpp
+++ algorithms_level1/3.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-using namespace std;
-enum enOddOrEven { Odd = 0, Even = 1, };
-
-int ReadPositiveNumber() {
-	int NUM;
-	cout << "ENTER INTGER NUMBER: ";
-	cin >> NUM;
-	return NUM;
-}
-
-enOddOrEven CheckNumber(int NUM) {
-	if (NUM % 2 == 0)
-	{
-		return enOddOrEven::Even;
-	}
-	else {
-		return enOddOrEven::Odd;
-	}
-}
-string PrintResult(enOddOrEven OddOrEven) {
-	if (OddOrEven == enOddOrEven::Even)
-	{
-		return  " IS EVEN";
-
-	}
-	else {
-		return  " IS ODD";
-
-	}
-}
-
-
-int main() {
-	int Num = ReadPositiveNumber();
-	cout << Num << PrintResult(CheckNumber(Num)) << endl;
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//enum enOddOrEven { Odd = 0, Even = 1, };
+//
+//int ReadPositiveNumber() {
+//	int NUM;
+//	cout << "ENTER INTGER NUMBER: ";
+//	cin >> NUM;
+//	return NUM;
+//}
+//
+//enOddOrEven CheckNumber(int NUM) {
+//	if (NUM % 2 == 0)
+//	{
+//		return enOddOrEven::Even;
+//	}
+//	else {
+//		return enOddOrEven::Odd;
+//	}
+//}
+//string PrintResult(enOddOrEven OddOrEven) {
+//	if (OddOrEven == enOddOrEven::Even)
+//	{
+//		return  " IS EVEN";
+//
+//	}
+//	else {
+//		return  " IS ODD";
+//
+//	}
+//}
+//
+//
+//int main() {
+//	int Num = ReadPositiveNumber();
+//	cout << Num << PrintResult(CheckNumber(Num)) << endl;
+//	return 0;
+//
+//}
--- algorithms_level1/4.cpp
+++ algorithms_level1/4.cpp
@@ -1,25 +1,51 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	short int AGE;
-//	bool isDriverLicense;
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	cout << "ENTER YOUR AGE: ";
-//	cin >> AGE;	
-//	cout << "ENTER Driver License, has it?1/0: ";
-//	cin >> isDriverLicense;
-//	cout << "#####################-by-ahmed-mady-####################\n";
-//	if (AGE>21&& isDriverLicense)
-//	{
-//		cout   << "YOU IS Hired" << endl;
-//	}
-//	else {
-//		cout << "YOU IS Rejected" << endl;
-//	}
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//
-//	return 0;
-//
-//}
+#include <iostream>
+using namespace std;
+
+
+struct stPersonInfo {
+	short int Age;
+	bool HasDriverLicense;
+};
+stPersonInfo ReadPersonInfo() {
+	stPersonInfo  PersonInfo;
+	cout << "ENTER YOUR AGE: ";
+	cin >> PersonInfo.Age;
+	cout << "ENTER Driver License, has it?1/0: ";
+	cin >> PersonInfo.HasDriverLicense;
+	return PersonInfo;
+}
+bool ChechPersonIsHired(stPersonInfo  PersonInfo) {
+	return ((PersonInfo.Age > 21) && PersonInfo.HasDriverLicense);
+}
+void PrintResult(bool IsAccepted) {
+	if (IsAccepted) {
+		cout << "YOU IS Hired" << endl;
+	}
+	else {
+		cout << "YOU IS Rejected" << endl;
+	}
+}
+
+
+int main() {
+	short int AGE;
+	bool isDriverLicense;
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	PrintResult(ChechPersonIsHired(ReadPersonInfo()));
+
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
