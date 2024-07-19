d9d95383926fbd9ab7ec6ba2ede195dee2a1230e
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-19 17:00:07 +0300

    FullName

--- algorithms_level1/5.cpp
+++ algorithms_level1/5.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-using namespace std;
-
-
-struct stPersonInfo {
-	short int Age;
-	bool HasDriverLicense;
-	bool HasRecommendation;
-};
-stPersonInfo ReadPersonInfo() {
-	stPersonInfo  PersonInfo;
-	cout << "ENTER YOUR AGE: ";
-	cin >> PersonInfo.Age;
-	cout << "ENTER Driver License, has it? ";
-	cin >> PersonInfo.HasDriverLicense;
-	cout << "ENTER Recommendation, has it? ";
-	cin >> PersonInfo.HasRecommendation;
-	return PersonInfo;
-}
-bool ChechPersonIsHired(stPersonInfo  PersonInfo) {
-	if (PersonInfo.HasRecommendation) {
-		return true;
-	}
-	else {
-		return ((PersonInfo.Age > 21) && PersonInfo.HasDriverLicense);
-	}
-}
-void PrintResult(bool IsAccepted) {
-	if (IsAccepted) {
-		cout << "YOU IS Hired" << endl;
-	}
-	else {
-		cout << "YOU IS Rejected" << endl;
-	}
-}
-
-
-int main() {
-	short int AGE;
-	bool isDriverLicense;
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	PrintResult(ChechPersonIsHired(ReadPersonInfo()));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//
+//struct stPersonInfo {
+//	short int Age;
+//	bool HasDriverLicense;
+//	bool HasRecommendation;
+//};
+//stPersonInfo ReadPersonInfo() {
+//	stPersonInfo  PersonInfo;
+//	cout << "ENTER YOUR AGE: ";
+//	cin >> PersonInfo.Age;
+//	cout << "ENTER Driver License, has it? ";
+//	cin >> PersonInfo.HasDriverLicense;
+//	cout << "ENTER Recommendation, has it? ";
+//	cin >> PersonInfo.HasRecommendation;
+//	return PersonInfo;
+//}
+//bool ChechPersonIsHired(stPersonInfo  PersonInfo) {
+//	if (PersonInfo.HasRecommendation) {
+//		return true;
+//	}
+//	else {
+//		return ((PersonInfo.Age > 21) && PersonInfo.HasDriverLicense);
+//	}
+//}
+//void PrintResult(bool IsAccepted) {
+//	if (IsAccepted) {
+//		cout << "YOU IS Hired" << endl;
+//	}
+//	else {
+//		cout << "YOU IS Rejected" << endl;
+//	}
+//}
+//
+//
+//int main() {
+//	short int AGE;
+//	bool isDriverLicense;
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	PrintResult(ChechPersonIsHired(ReadPersonInfo()));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
--- algorithms_level1/6.cpp
+++ algorithms_level1/6.cpp
@@ -1,23 +1,32 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	string FristName;
-//	string LastName;
-//
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	cout << "ENTER YOUR FRIST NAME: ";
-//	cin >> FristName;
-//	cout << "ENTER YOUR LAST NAME: ";
-//	 	cin >> LastName;
-// 
-//	cout << "#####################-by-ahmed-mady-####################\n";
-// 
-//		cout << FristName<< " "<< LastName << endl;
-// 
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//
-//	return 0;
-//
-//}
+#include <iostream>
+using namespace std;
+string ReadName(string Message) {
+	string  Name;
+	cout << Message;
+	cin >> Name;
+	return Name;
+}
+string ReadData() {
+	string FristName = ReadName("ENTER YOUR FRIST NAME: ");
+	string LastName = ReadName("ENTER YOUR LAST NAME: ");
+	return  FristName + " " + LastName;
+}
+
+
+void PrintFullName(string FullName) {
+	cout << FullName << endl;
+
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	PrintFullName(ReadData());
+
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+
+	return 0;
+
+}
