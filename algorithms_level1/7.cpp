7cba4eec4875695e2ef8065c8484d39dc56a4f10
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-19 17:32:08 +0300

    CheckMark

--- algorithms_level1/7.cpp
+++ algorithms_level1/7.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-int ReadPositiveNumber() {
-	int NUM;
-	do {
-		cout << "ENTER POSITIVE NUMBER: ";
-		cin >> NUM;
-	} while (NUM <= 0);
-	return NUM;
-}
-float CalculateHalfOfNumber(int Number) {
-	float halfNumber = (float) Number / 2;
-	return halfNumber;
-}
-void PrintHalfOfNumber(int Number, float halfNumber) {
-	cout << "#### Half of the " << to_string(Number) << " is " << to_string(halfNumber)<< "\t\t\t\t#############" << endl;
-}
-int main() {
-	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
-	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
-	
-	int Number = ReadPositiveNumber();
-	PrintHalfOfNumber(Number, CalculateHalfOfNumber(Number));
-	
-	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
-	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
-
-	return 0;
-
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//
+//int ReadPositiveNumber() {
+//	int NUM;
+//	do {
+//		cout << "ENTER POSITIVE NUMBER: ";
+//		cin >> NUM;
+//	} while (NUM <= 0);
+//	return NUM;
+//}
+//float CalculateHalfOfNumber(int Number) {
+//	float halfNumber = (float) Number / 2;
+//	return halfNumber;
+//}
+//void PrintHalfOfNumber(int Number, float halfNumber) {
+//	cout << "#### Half of the " << to_string(Number) << " is " << to_string(halfNumber)<< "\t\t\t\t#############" << endl;
+//}
+//int main() {
+//	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
+//	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
+//	
+//	int Number = ReadPositiveNumber();
+//	PrintHalfOfNumber(Number, CalculateHalfOfNumber(Number));
+//	
+//	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
+//	cout << "##############\t#############\t-by-ahmed-mady-\t################\t#############\n";
+//
+//	return 0;
+//
+//}
--- algorithms_level1/8.cpp
+++ algorithms_level1/8.cpp
@@ -1,28 +1,49 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	short int MARK;
-//
-//	cout << "##############\t#############\t################\t#############\n";
-//	cout << "##############\t#############\t################\t#############\n";
-//	cout << "#### ENTER MARK: ";
-//	if (!(cin >> MARK)||( MARK>100)) {
-//		cout << "####  invalid input" << "\t\t\t\t#############" << endl;
-//		return 0;
-//	}
-//	cout << "#####################-by-ahmed-mady-####################\n";
-//
-//	if (MARK >= 50) {
-//		cout << "####  PASS" << "\t\t\t\t#############" << endl;
-//
-//	}
-//	else {
-//		cout << "####  FAIL" << "\t\t\t\t#############" << endl;
-//
-//	}
-//	cout << "##############\t#############\t################\t#############\n";
-//	cout << "##############\t#############\t################\t#############\n";
-//
-//	return 0;
-//
-//}
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum enMarkStatus {
+	Fail = 0,
+	Pass = 1,
+};
+
+int ReadNumberInRange(int From, int To, string Message = "ENTER NUMBER") {
+	int NUM;
+	do {
+		cout << Message << " FROM " << to_string(From) << " TO " << to_string(To) << ": ";
+		cin >> NUM;
+	} while (NUM < From && NUM > To);
+	return NUM;
+}
+enMarkStatus CheckMark(int Mark) {
+	if (Mark > 50) {
+		return enMarkStatus::Pass;
+	}
+	else {
+		return enMarkStatus::Fail;
+	}
+
+}
+void PrintMarkResult(enMarkStatus MarkStatus) {
+	if (MarkStatus == enMarkStatus::Pass) {
+		cout << "####  PASS" << "\t\t\t\t#############" << endl;
+	}
+	else {
+		cout << "####  FAIL" << "\t\t\t\t#############" << endl;
+	}
+}
+
+int main() {
+	short int MARK;
+
+	cout << "##############\t#############\t################\t#############\n";
+	cout << "##############\t#############\t################\t#############\n";
+
+	PrintMarkResult(CheckMark(ReadNumberInRange(0, 100, "ENTER MARK")));
+
+	cout << "##############\t#############\t################\t#############\n";
+	cout << "##############\t#############\t################\t#############\n";
+
+	return 0;
+
+}
