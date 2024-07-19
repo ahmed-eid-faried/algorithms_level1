77579a44ff5dc23e8216c9e49f9fa34956485b69
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-19 16:37:48 +0300

    OddOrEven

--- algorithms_level1/3.cpp
+++ algorithms_level1/3.cpp
@@ -1,22 +1,39 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	int NUM;
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	cout << "ENTER INTGER NUMBER: ";
-//	cin >> NUM;
-//	cout << "#####################-by-ahmed-mady-####################\n";
-//	if (NUM % 2 == 0)
-//	{
-//		cout << NUM << " IS EVEN" << endl;
-//	}
-//	else {
-//		cout << NUM << " IS ODD" << endl;
-//	}
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//
-//	return 0;
-//
-//}
+#include <iostream>
+using namespace std;
+enum enOddOrEven { Odd = 0, Even = 1, };
+
+int ReadPositiveNumber() {
+	int NUM;
+	cout << "ENTER INTGER NUMBER: ";
+	cin >> NUM;
+	return NUM;
+}
+
+enOddOrEven CheckNumber(int NUM) {
+	if (NUM % 2 == 0)
+	{
+		return enOddOrEven::Even;
+	}
+	else {
+		return enOddOrEven::Odd;
+	}
+}
+string PrintResult(enOddOrEven OddOrEven) {
+	if (OddOrEven == enOddOrEven::Even)
+	{
+		return  " IS EVEN";
+
+	}
+	else {
+		return  " IS ODD";
+
+	}
+}
+
+
+int main() {
+	int Num = ReadPositiveNumber();
+	cout << Num << PrintResult(CheckNumber(Num)) << endl;
+	return 0;
+
+}
