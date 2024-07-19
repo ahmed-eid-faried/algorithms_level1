c3b45fdc502e9553d38c4c2edb9cd41e94616ca5
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-19 17:43:16 +0300

    CalculateAverageNumbers

--- algorithms_level1/9.cpp
+++ algorithms_level1/9.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-using namespace std;
-
-float ReadPositiveNumber(string Message) {
-	float NUM;
-	do {
-		cout << Message;
-		cin >> NUM;
-	} while (NUM < 0);
-	return NUM;
-}
-
-
-float CalculateSumNumbers() {
-	float Num1 = ReadPositiveNumber("ENTER POSITIVE NUMBER 1: ");
-	float Num2 = ReadPositiveNumber("ENTER POSITIVE NUMBER 2: ");
-	float Num3 = ReadPositiveNumber("ENTER POSITIVE NUMBER 3: ");
-	return Num1 + Num2 + Num3;
-}
-
-void PrintSumNumbers(float Sum) {
-	cout << " Sum is " << (Sum) << endl;
-
-}
-int main() {
-	cout << "##############\t#############\t################\t#############\n";
-	cout << "##############\t#############\t################\t#############\n";
-
-	PrintSumNumbers(CalculateSumNumbers());
-
-	cout << "##############\t#############\t################\t#############\n";
-	cout << "##############\t#############\t################\t#############\n";
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//float ReadPositiveNumber(string Message) {
+//	float NUM;
+//	do {
+//		cout << Message;
+//		cin >> NUM;
+//	} while (NUM < 0);
+//	return NUM;
+//}
+//
+//
+//float CalculateSumNumbers() {
+//	float Num1 = ReadPositiveNumber("ENTER POSITIVE NUMBER 1: ");
+//	float Num2 = ReadPositiveNumber("ENTER POSITIVE NUMBER 2: ");
+//	float Num3 = ReadPositiveNumber("ENTER POSITIVE NUMBER 3: ");
+//	return Num1 + Num2 + Num3;
+//}
+//
+//void PrintSumNumbers(float Sum) {
+//	cout << " Sum is " << (Sum) << endl;
+//
+//}
+//int main() {
+//	cout << "##############\t#############\t################\t#############\n";
+//	cout << "##############\t#############\t################\t#############\n";
+//
+//	PrintSumNumbers(CalculateSumNumbers());
+//
+//	cout << "##############\t#############\t################\t#############\n";
+//	cout << "##############\t#############\t################\t#############\n";
+//	return 0;
+//
+//}
