8bbef496c977c547559dbfb751da40e1e6cfed51
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 19:33:20 +0300

    Number^M

--- algorithms_level1/31.cpp
+++ algorithms_level1/31.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-using namespace std;
-int  ReadPositiveNumber() {
-	int Num;
-	do {
-		cout << "ENTER POSITIVE NUMBER: ";
-		cin >> Num;
-	} while (Num < 0);
-	return Num;
-}
-void PrintResult(int Num, string title) {
-	cout << title << "====>>> " << Num << endl;
-}
-
-void PrintCalculatePowerOfNumber(int Num) {
-	PrintResult(Num * Num, " Number^2 :");
-	cout << "---------------------------\n";
-	PrintResult(Num * Num * Num, " Number^3 :");
-	cout << "---------------------------\n";
-	PrintResult(Num * Num * Num * Num, " Number^4 :");
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
- 	PrintCalculatePowerOfNumber(ReadPositiveNumber());
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//int  ReadPositiveNumber() {
+//	int Num;
+//	do {
+//		cout << "ENTER POSITIVE NUMBER: ";
+//		cin >> Num;
+//	} while (Num < 0);
+//	return Num;
+//}
+//void PrintResult(int Num, string title) {
+//	cout << title << "====>>> " << Num << endl;
+//}
+//
+//void PrintCalculatePowerOfNumber(int Num) {
+//	PrintResult(Num * Num, "Number^2 :");
+//	cout << "---------------------------\n";
+//	PrintResult(Num * Num * Num, "Number^3 :");
+//	cout << "---------------------------\n";
+//	PrintResult(Num * Num * Num * Num, "Number^4 :");
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+// 	PrintCalculatePowerOfNumber(ReadPositiveNumber());
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
