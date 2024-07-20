c66a69ac95dd4640223553e338d54243606a05ed
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 08:29:18 +0300

    CalculateRectangleAreaThroughDiagonal

--- algorithms_level1/14.cpp
+++ algorithms_level1/14.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-using namespace std;
-
-
-
-void ReadNumbers(int& Num1, int& Num2) {
-	cout << "ENTER Num1 : ";
-	if (!(cin >> Num1)) {
-		cout << " invalid input" << endl;
-	}
-	cout << "ENTER Num2: ";
-	if (!(cin >> Num2)) {
-		cout << " invalid input" << endl;
-	}
-}
-void Swap2Numbers(int& Num1, int& Num2) {
-	int Temp;
-	Temp = Num1;
-	Num1 = Num2;
-	Num2 = Temp;
-}
-void PrintNumbers(int  Num1, int  Num2, string Message) {
-	cout << Message << endl;
-	cout << Num1 << endl;
-	cout << Num2 << endl;
-}
-int main() {
-
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	int Num1;
-	int Num2;
-	ReadNumbers(Num1, Num2);
-	PrintNumbers(Num1, Num2, "old:");
-	cout << "########################################################\n";
-	Swap2Numbers(Num1, Num2);
-	PrintNumbers(Num1, Num2, "new:");
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//
+//
+//void ReadNumbers(int& Num1, int& Num2) {
+//	cout << "ENTER Num1 : ";
+//	if (!(cin >> Num1)) {
+//		cout << " invalid input" << endl;
+//	}
+//	cout << "ENTER Num2: ";
+//	if (!(cin >> Num2)) {
+//		cout << " invalid input" << endl;
+//	}
+//}
+//void Swap2Numbers(int& Num1, int& Num2) {
+//	int Temp;
+//	Temp = Num1;
+//	Num1 = Num2;
+//	Num2 = Temp;
+//}
+//void PrintNumbers(int  Num1, int  Num2, string Message) {
+//	cout << Message << endl;
+//	cout << Num1 << endl;
+//	cout << Num2 << endl;
+//}
+//int main() {
+//
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	int Num1;
+//	int Num2;
+//	ReadNumbers(Num1, Num2);
+//	PrintNumbers(Num1, Num2, "old:");
+//	cout << "########################################################\n";
+//	Swap2Numbers(Num1, Num2);
+//	PrintNumbers(Num1, Num2, "new:");
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//
+//	return 0;
+//
+//}
--- algorithms_level1/15.cpp
+++ algorithms_level1/15.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-using namespace std;
-float ReadPositiveNumber(string Message) {
-	float NUM;
-	do {
-		cout << Message;
-		cin >> NUM;
-	} while (NUM < 0);
-	return NUM;
-}
-void ReadRectangleData(float& Hieght, float& Base) {
-	Hieght = ReadPositiveNumber("ENTER Hieght: ");
-	Base = ReadPositiveNumber("ENTER Base: ");
-}
-float CalculateRectangleArea(float  Hieght, float  Base) {
-	return Hieght * Base;
-}
-void PrintRectangleArea(float RectangleArea) {
-	cout << "RectangleArea is " << RectangleArea << endl;
-
-}
-
-
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	float Hieght;
-	float Base;
-	ReadRectangleData(Hieght, Base);
-	PrintRectangleArea(CalculateRectangleArea(Hieght, Base));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//float ReadPositiveNumber(string Message) {
+//	float NUM;
+//	do {
+//		cout << Message;
+//		cin >> NUM;
+//	} while (NUM < 0);
+//	return NUM;
+//}
+//void ReadRectangleData(float& Hieght, float& Base) {
+//	Hieght = ReadPositiveNumber("ENTER Hieght: ");
+//	Base = ReadPositiveNumber("ENTER Base: ");
+//}
+//float CalculateRectangleArea(float  Hieght, float  Base) {
+//	return Hieght * Base;
+//}
+//void PrintRectangleArea(float RectangleArea) {
+//	cout << "RectangleArea is " << RectangleArea << endl;
+//
+//}
+//
+//
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	float Hieght;
+//	float Base;
+//	ReadRectangleData(Hieght, Base);
+//	PrintRectangleArea(CalculateRectangleArea(Hieght, Base));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//
+//}
