670302cd60cbfb2a9097a5837ff70859b7363c02
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 05:53:13 +0300

    CalculateAverageMarks

--- algorithms_level1/10.cpp
+++ algorithms_level1/10.cpp
@@ -1,39 +1,39 @@
-
-#include <iostream>
-using namespace std;
-
-float ReadPositiveNumber(string Message) {
-	float NUM;
-	do {
-		cout << Message;
-		cin >> NUM;
-	} while (NUM < 0);
-	return NUM;
-}
-
-
-float CalculateAverageNumbers() {
-	float Num1 = ReadPositiveNumber("ENTER POSITIVE NUMBER 1: ");
-	float Num2 = ReadPositiveNumber("ENTER POSITIVE NUMBER 2: ");
-	float Num3 = ReadPositiveNumber("ENTER POSITIVE NUMBER 3: ");
-	return (Num1 + Num2 + Num3) / 3;
-}
-
-void PrintAverageNumbers(float Average) {
-	cout << " Average is " << (Average) << endl;
-
-}
-int main() {
-	cout << "##############\t#############\t################\t#############\n";
-	cout << "##############\t#############\t################\t#############\n";
-
-	PrintAverageNumbers(CalculateAverageNumbers());
-
-	cout << "##############\t#############\t################\t#############\n";
-	cout << "##############\t#############\t################\t#############\n";
-	return 0;
-
-}
+//
+//#include <iostream>
+//using namespace std;
+//
+//float ReadPositiveNumber(string Message) {
+//	float NUM;
+//	do {
+//		cout << Message;
+//		cin >> NUM;
+//	} while (NUM < 0);
+//	return NUM;
+//}
+//
+//
+//float CalculateAverageNumbers() {
+//	float Num1 = ReadPositiveNumber("ENTER POSITIVE NUMBER 1: ");
+//	float Num2 = ReadPositiveNumber("ENTER POSITIVE NUMBER 2: ");
+//	float Num3 = ReadPositiveNumber("ENTER POSITIVE NUMBER 3: ");
+//	return (Num1 + Num2 + Num3) / 3;
+//}
+//
+//void PrintAverageNumbers(float Average) {
+//	cout << " Average is " << (Average) << endl;
+//
+//}
+//int main() {
+//	cout << "##############\t#############\t################\t#############\n";
+//	cout << "##############\t#############\t################\t#############\n";
+//
+//	PrintAverageNumbers(CalculateAverageNumbers());
+//
+//	cout << "##############\t#############\t################\t#############\n";
+//	cout << "##############\t#############\t################\t#############\n";
+//	return 0;
+//
+//}
 
 
 
--- algorithms_level1/11.cpp
+++ algorithms_level1/11.cpp
@@ -1,3 +1,72 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float ReadNumberInRange(int From, int To, string Message = "ENTER NUMBER") {
+	float NUM;
+	do {
+		cout << Message << " FROM " << to_string(From) << " TO " << to_string(To) << ": ";
+		cin >> NUM;
+	} while (NUM < From || NUM > To);
+	return NUM;
+}
+
+float CalculateAverageMarks() {
+	float Mark1 = ReadNumberInRange(0, 100, "ENTER MARK 1");
+	float Mark2 = ReadNumberInRange(0, 100, "ENTER MARK 2");
+	float Mark3 = ReadNumberInRange(0, 100, "ENTER MARK 3");
+	return (Mark1 + Mark2 + Mark3) / 3;
+}
+
+void PrintAverageMarks(float Average) {
+	cout << " Average is " << (Average) << endl;
+	if (Average > 50) {
+		cout << " PASS " << endl;
+	}
+	else {
+		cout << " FAIL " << endl;
+	}
+}
+int main() {
+	cout << "##############\t#############\t################\t#############\n";
+	cout << "##############\t#############\t################\t#############\n";
+	PrintAverageMarks(CalculateAverageMarks());
+	cout << "##############\t#############\t################\t#############\n";
+	cout << "##############\t#############\t################\t#############\n";
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //#include <iostream>
 //using namespace std;
 //int main() {
