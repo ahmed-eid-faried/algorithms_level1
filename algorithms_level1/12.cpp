131ff381bb81950407de3f28f1dbe764eca50a54
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 06:10:54 +0300

    FindMaxNumbersFrom3

--- algorithms_level1/12.cpp
+++ algorithms_level1/12.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-using namespace std;
-
-void ReadNumber(int& Num1, int& Num2) {
-	cout << "ENTER Num1 : ";
-	if (!(cin >> Num1)) {
-		cout << " invalid input" << endl;
-	}
-	cout << "ENTER Num2: ";
-	if (!(cin >> Num2)) {
-		cout << " invalid input" << endl;
-	}
-}
-void FindMaxNumber(int  Num1, int  Num2) {
-	if (Num1 > Num2) {
-		cout << "MAX IS " << Num1 << endl;
-	}
-	else {
-		cout << "MAX IS " << Num2 << endl;
-	}
-}
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	int Num1;
-	int Num2;
-	ReadNumber(Num1, Num2);
-	FindMaxNumber(Num1, Num2);
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//void ReadNumber(int& Num1, int& Num2) {
+//	cout << "ENTER Num1 : ";
+//	if (!(cin >> Num1)) {
+//		cout << " invalid input" << endl;
+//	}
+//	cout << "ENTER Num2: ";
+//	if (!(cin >> Num2)) {
+//		cout << " invalid input" << endl;
+//	}
+//}
+//void FindMaxNumber(int  Num1, int  Num2) {
+//	if (Num1 > Num2) {
+//		cout << "MAX IS " << Num1 << endl;
+//	}
+//	else {
+//		cout << "MAX IS " << Num2 << endl;
+//	}
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	int Num1;
+//	int Num2;
+//	ReadNumber(Num1, Num2);
+//	FindMaxNumber(Num1, Num2);
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//
+//	return 0;
+//
+//}
--- algorithms_level1/13.cpp
+++ algorithms_level1/13.cpp
@@ -1,55 +1,55 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	int Num1;
-//	int Num2;
-//	int Num3;
-//	int Max;
-//
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	cout << "ENTER Num1 : ";
-//	if (!(cin >> Num1)) {
-//		cout << " invalid input" << endl;
-//		return 0;
-//	}
-//	cout << "ENTER Num2: ";
-//	if (!(cin >> Num2)) {
-//		cout << " invalid input" << endl;
-//		return 0;
-//	}
-//	cout << "ENTER Num3: ";
-//	if (!(cin >> Num3)) {
-//		cout << " invalid input" << endl;
-//		return 0;
-//	}
-//
-//	cout << "#####################-by-ahmed-mady-####################\n";
-//	if (Num1 > Num2) {
-//		if (Num1 > Num3) {
-//			Max = Num1;
-//		}
-//		else {
-//			Max = Num3;
-//
-//		}
-//	}
-//	else {
-//		if (Num2 > Num3) {
-//			Max = Num2;
-//		}
-//		else {
-//			Max = Num3;
-//
-//		}
-//	}
-// 
-//	cout << Max << endl;
-//
-//
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//
-//	return 0;
-//
-//}
+#include <iostream>
+using namespace std;
+void ReadNumbers(int& Num1, int& Num2, int& Num3) {
+	cout << "ENTER Num1 : ";
+	if (!(cin >> Num1)) {
+		cout << " invalid input" << endl;
+	}
+	cout << "ENTER Num2: ";
+	if (!(cin >> Num2)) {
+		cout << " invalid input" << endl;
+	}
+	cout << "ENTER Num3: ";
+	if (!(cin >> Num3)) {
+		cout << " invalid input" << endl;
+	}
+}
+int FindMaxNumbersFrom3(int Num1, int Num2, int Num3) {
+	int Max;
+	if (Num1 > Num2) {
+		if (Num1 > Num3) {
+			Max = Num1;
+		}
+		else {
+			Max = Num3;
+		}
+	}
+	else {
+		if (Num2 > Num3) {
+			Max = Num2;
+		}
+		else {
+			Max = Num3;
+		}
+	}
+	return Max;
+
+}
+void PrintMaxNumber(int Max) {
+	cout << "MAX IS " << Max << endl;
+}
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	int Num1;
+	int Num2;
+	int Num3;
+	ReadNumbers(Num1, Num2, Num3);
+	PrintMaxNumber(FindMaxNumbersFrom3(Num1, Num2, Num3));
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+
+	return 0;
+
+}
