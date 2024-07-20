f7810068501a5d0dd60e32cde1a74e5f827dfbfa
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 09:56:37 +0300

    CalculateCircleAreaInscribedInASquare

--- algorithms_level1/19.cpp
+++ algorithms_level1/19.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-using namespace std;
-float ReadPositiveNumber(string Message) {
-	float NUM;
-	do {
-		cout << Message;
-		cin >> NUM;
-	} while (NUM < 0);
-	return NUM;
-} 
-
-double CalculateCircleArea(float  Diameter) {
-	const double Pi = 3.14159265359;
-	return (Pi * Diameter * Diameter)/4;
-}
-
-void PrintCircleArea(double CircleArea) {
-	cout << "Triangle Area is " << CircleArea << endl;
-}
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	PrintCircleArea(CalculateCircleArea(ReadPositiveNumber("Enter Diameter: ")));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//float ReadPositiveNumber(string Message) {
+//	float NUM;
+//	do {
+//		cout << Message;
+//		cin >> NUM;
+//	} while (NUM < 0);
+//	return NUM;
+//} 
+//
+//double CalculateCircleArea(float  Diameter) {
+//	const double Pi = 3.14159265359;
+//	return (Pi * Diameter * Diameter)/4;
+//}
+//
+//void PrintCircleArea(double CircleArea) {
+//	cout << "Triangle Area is " << CircleArea << endl;
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	PrintCircleArea(CalculateCircleArea(ReadPositiveNumber("Enter Diameter: ")));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//
+//}
