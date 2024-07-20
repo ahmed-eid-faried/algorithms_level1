85c6020a8b15b389e165ed13ad6b14053d3569cc
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 06:26:08 +0300

    Swap2Numbers

--- algorithms_level1/14.cpp
+++ algorithms_level1/14.cpp
@@ -1,40 +1,44 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	int Num1;
-//	int Num2;
-// 
-//
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	cout << "ENTER Num1 : ";
-//	if (!(cin >> Num1)) {
-//		cout << " invalid input" << endl;
-//		return 0;
-//	}
-//	cout << "ENTER Num2: ";
-//	if (!(cin >> Num2)) {
-//		cout << " invalid input" << endl;
-//		return 0;
-//	}
-// 
-//
-//	cout << "#####################-by-ahmed-mady-####################\n";
-// 
-//
-//	cout <<"old:" << endl;
-//	cout << Num1 << endl;
-//	cout << Num2 << endl;
-//	cout << "########################################################\n";
-//	cout << "new:" << endl;
-//
-//	cout << Num2 << endl;
-//	cout << Num1 << endl;
-//
-//
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//
-//	return 0;
-//
-//}
+#include <iostream>
+using namespace std;
+
+
+
+void ReadNumbers(int& Num1, int& Num2) {
+	cout << "ENTER Num1 : ";
+	if (!(cin >> Num1)) {
+		cout << " invalid input" << endl;
+	}
+	cout << "ENTER Num2: ";
+	if (!(cin >> Num2)) {
+		cout << " invalid input" << endl;
+	}
+}
+void Swap2Numbers(int& Num1, int& Num2) {
+	int Temp;
+	Temp = Num1;
+	Num1 = Num2;
+	Num2 = Temp;
+}
+void PrintNumbers(int  Num1, int  Num2, string Message) {
+	cout << Message << endl;
+	cout << Num1 << endl;
+	cout << Num2 << endl;
+}
+int main() {
+
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	int Num1;
+	int Num2;
+	ReadNumbers(Num1, Num2);
+	PrintNumbers(Num1, Num2, "old:");
+	cout << "########################################################\n";
+	Swap2Numbers(Num1, Num2);
+	PrintNumbers(Num1, Num2, "new:");
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+
+	return 0;
+
+}
