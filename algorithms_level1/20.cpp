03edb7f71e409c3ab8c09b6e9814490e46b6bf00
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-20 10:08:42 +0300

    CalculateCircleAreaAlongTheCircumference

--- algorithms_level1/20.cpp
+++ algorithms_level1/20.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-using namespace std;
-float ReadPositiveNumber(string Message) {
-	float NUM;
-	do {
-		cout << Message;
-		cin >> NUM;
-	} while (NUM < 0);
-	return NUM;
-}
-
-double CalculateCircleAreaInscribedInASquare(float  SideOfSquare) {
-	const double Pi = 3.14159265359;
-	return (Pi * SideOfSquare * SideOfSquare) / 4;
-}
-
-void PrintCircleArea(double CircleArea) {
-	cout << "Triangle Area is " << CircleArea << endl;
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	PrintCircleArea(CalculateCircleAreaInscribedInASquare(ReadPositiveNumber("Enter Side Of Square: ")));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//float ReadPositiveNumber(string Message) {
+//	float NUM;
+//	do {
+//		cout << Message;
+//		cin >> NUM;
+//	} while (NUM < 0);
+//	return NUM;
+//}
+//
+//double CalculateCircleAreaInscribedInASquare(float  SideOfSquare) {
+//	const double Pi = 3.14159265359;
+//	return (Pi * SideOfSquare * SideOfSquare) / 4;
+//}
+//
+//void PrintCircleArea(double CircleArea) {
+//	cout << "Triangle Area is " << CircleArea << endl;
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	PrintCircleArea(CalculateCircleAreaInscribedInASquare(ReadPositiveNumber("Enter Side Of Square: ")));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//
+//}
---
+++ algorithms_level1/21.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+float ReadPositiveNumber(string Message) {
+	float NUM;
+	do {
+		cout << Message;
+		cin >> NUM;
+	} while (NUM < 0);
+	return NUM;
+}
+
+double CalculateCircleAreaAlongTheCircumference(float  Circumference) {
+	const double Pi = 3.14159265359;
+	return (Circumference * Circumference) / (Pi * 4);
+}
+
+void PrintCircleArea(double CircleArea) {
+	cout << "Triangle Area is " << CircleArea << endl;
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	PrintCircleArea(CalculateCircleAreaAlongTheCircumference(ReadPositiveNumber("Enter Circumference: ")));
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+
+}
+
