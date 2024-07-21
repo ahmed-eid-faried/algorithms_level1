5c81e3ac0f7e2c532980c9f6ed367b3bf9d1de3a
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 08:08:43 +0300

    CalculateNumberOfDaysAndWeeks

--- algorithms_level1/40.cpp
+++ algorithms_level1/40.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-using namespace std;
-
-double  ReadPositiveNumber(string Message) {
-	double Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return Num;
-}
-void PrintResult(double Remainder, string Message) {
-	cout << "---------------------------\n";
-	cout << Message << (double)Remainder << endl;
-}
-
-void CalculateRemainder(double BillValue) {
-	double SalesTax = BillValue * 0.1;
-	PrintResult(SalesTax, "SalesTax: ");
-	double ServicesFee = BillValue * 1.1 * 0.16;
-	PrintResult(ServicesFee, "ServicesFee: ");
-	double ServicesFeeAndSalesTax = ServicesFee + SalesTax;
-	PrintResult(ServicesFeeAndSalesTax, "ServicesFeeAndSalesTax: ");
-	double TotalAferServicesFeeAndSalesTax = BillValue * 1.1 * 1.16;
-	PrintResult(TotalAferServicesFeeAndSalesTax, "TotalAferServicesFeeAndSalesTax: ");
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	CalculateRemainder(ReadPositiveNumber("Enter BillValue: "));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//double  ReadPositiveNumber(string Message) {
+//	double Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return Num;
+//}
+//void PrintResult(double Num, string Message) {
+//	cout << "---------------------------\n";
+//	cout << Message << (double)Num << endl;
+//}
+//
+//void CalculateTotalAferServicesFeeAndSalesTax(double BillValue) {
+//	double SalesTax = BillValue * 0.1;
+//	PrintResult(SalesTax, "SalesTax: ");
+//	double ServicesFee = BillValue * 1.1 * 0.16;
+//	PrintResult(ServicesFee, "ServicesFee: ");
+//	double ServicesFeeAndSalesTax = ServicesFee + SalesTax;
+//	PrintResult(ServicesFeeAndSalesTax, "ServicesFeeAndSalesTax: ");
+//	double TotalAferServicesFeeAndSalesTax = BillValue * 1.1 * 1.16;
+//	PrintResult(TotalAferServicesFeeAndSalesTax, "TotalAferServicesFeeAndSalesTax: ");
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	CalculateTotalAferServicesFeeAndSalesTax(ReadPositiveNumber("Enter BillValue: "));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
