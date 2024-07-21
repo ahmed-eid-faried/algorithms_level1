d524377436de36221e0d3aaba4d12a84ee4d8e4c
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 08:24:05 +0300

    CalculateDurationInSeconds

--- algorithms_level1/41.cpp
+++ algorithms_level1/41.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-using namespace std;
-
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return Num;
-}
-void PrintResult(double Num, string Message) {
-	cout << "---------------------------\n";
-	cout << Message << Num << endl;
-}
-
-void CalculateNumberOfDaysAndWeeks(int NumberOfHours) {
-	double NumberOfDays = (double)NumberOfHours / 24;
-	PrintResult(NumberOfDays, "NumberOfDays: ");
-	double NumberOfWeeks = (double)NumberOfHours / 24 / 7;
-	PrintResult(NumberOfWeeks, "NumberOWeeks: ");
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	CalculateNumberOfDaysAndWeeks(ReadPositiveNumber("Enter BillValue: "));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return Num;
+//}
+//void PrintResult(double Num, string Message) {
+//	cout << "---------------------------\n";
+//	cout << Message << Num << endl;
+//}
+//
+//void CalculateNumberOfDaysAndWeeks(int NumberOfHours) {
+//	double NumberOfDays = (double)NumberOfHours / 24;
+//	PrintResult(NumberOfDays, "NumberOfDays: ");
+//	double NumberOfWeeks = (double)NumberOfHours / 24 / 7;
+//	PrintResult(NumberOfWeeks, "NumberOWeeks: ");
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	CalculateNumberOfDaysAndWeeks(ReadPositiveNumber("Enter BillValue: "));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
---
+++ algorithms_level1/42.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+struct stDuration {
+	int days, hours, minutes, seconds;
+};
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return Num;
+}
+
+
+void ReadDuration(stDuration& Duration) {
+	Duration.days = ReadPositiveNumber("Enter days: ");
+	Duration.hours = ReadPositiveNumber("Enter hours: ");
+	Duration.minutes = ReadPositiveNumber("Enter minutes: ");
+	Duration.seconds = ReadPositiveNumber("Enter seconds: ");
+}
+void PrintResult(double Num, string Message) {
+	cout << "---------------------------\n";
+	cout << Message << Num << endl;
+}
+
+void CalculateDurationInSeconds(stDuration  Duration) {
+	int NumberOfsecondsInSeconds = Duration.seconds;
+	PrintResult(NumberOfsecondsInSeconds, "NumberOfsecondsInSeconds: ");
+
+	int NumberOfsecondsInMinutes = Duration.minutes * 60;
+	PrintResult(NumberOfsecondsInMinutes, "NumberOfsecondsInMinutes: ");
+
+	int NumberOfsecondsInHours = Duration.hours * 60 * 60;
+	PrintResult(NumberOfsecondsInHours, "NumberOfsecondsInHours: ");
+
+	int NumberOfsecondsInDays = Duration.days * 60 * 60 * 24;
+	PrintResult(NumberOfsecondsInDays, "NumberOfsecondsInDays: ");
+
+	cout << "---------------------------\n";
+	int Totalseconds = NumberOfsecondsInSeconds +
+		NumberOfsecondsInMinutes +
+		NumberOfsecondsInHours +
+		NumberOfsecondsInDays;
+	PrintResult(Totalseconds, "Totalseconds: ");
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	stDuration  Duration;
+	ReadDuration(Duration);
+	CalculateDurationInSeconds(Duration);
+ 	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
