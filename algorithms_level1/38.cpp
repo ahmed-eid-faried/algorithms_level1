8866c42d77c373c0418a4abcbebb240c27b1a283
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 11:48:00 +0300

    fix CheckPrimeNumber

--- algorithms_level1/38.cpp
+++ algorithms_level1/38.cpp
@@ -1,59 +1,60 @@
-//#include <iostream>
-//using namespace std;
-//enum enPrimeStatus { Prime, NotPrime };
-//
-//int  ReadPositiveNumber(string Message) {
-//	int Num;
-//	do {
-//		cout << Message;
-//		cin >> Num;
-//	} while (Num < 0);
-//	return Num;
-//}
-//void PrintPrimeNumber(enPrimeStatus  PrimeStatus, int Num) {
-//	cout << "---------------------------\n";
-//	if (PrimeStatus == enPrimeStatus::Prime) {
-//		cout << "Prime: " << Num << endl;
-//	}
-//	else {
-//		cout << "Not Prime: " << Num << endl;
-//	}
-//}
-//bool CheckEvenNumber(int Num) {
-//	return Num % 2 == 0;
-//}
-//bool CheckNumberIs0or1(int Num) {
-//	return(Num == 0 || Num == 1);
-//}
-//enPrimeStatus CheckPrimeNumber(int Num) {
-//	if (CheckEvenNumber(Num)) {
-//		return enPrimeStatus::NotPrime;
-//	}
-//	else
-//		if (CheckNumberIs0or1(Num)) {
-//			return enPrimeStatus::Prime;
-//		}
-//		else
-//		{
-//
-//			for (int i = 2; i < Num; i++) {
-//				if (Num % i == 0) {
-//					return enPrimeStatus::NotPrime;
-//				}
-//			}
-//			return enPrimeStatus::Prime;
-//		}
-//}
-//
-//int main() {
-//	cout << "########################################################\n";
-//	cout << "#####################-by-ahmed-mady-####################\n";
-//	cout << "########################################################\n";
-//	int Num;
-//	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
-//	enPrimeStatus  PrimeStatus = CheckPrimeNumber(Num);
-//	PrintPrimeNumber(PrimeStatus, Num);
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+enum enPrimeStatus { Prime, NotPrime };
+
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return Num;
+}
+void PrintPrimeNumber(enPrimeStatus  PrimeStatus, int Num) {
+	cout << "---------------------------\n";
+	if (PrimeStatus == enPrimeStatus::Prime) {
+		cout << "Prime: " << Num << endl;
+	}
+	else {
+		//cout << "Not Prime: " << Num << endl;
+		cout << "";
+	}
+}
+bool CheckEvenNumber(int Num) {
+	return Num % 2 == 0;
+}
+bool CheckNumberIs0or1Or2(int Num) {
+	return(Num == 0 || Num == 1 || Num == 2);
+}
+enPrimeStatus CheckPrimeNumber(int Num) {
+	if (CheckNumberIs0or1Or2(Num)) {
+		return enPrimeStatus::Prime;
+
+	}
+	else
+		if (CheckEvenNumber(Num)) {
+			return enPrimeStatus::NotPrime;
+		}
+		else
+		{
+			for (int i = 2; i < Num; i++) {
+				if (Num % i == 0) {
+					return enPrimeStatus::NotPrime;
+				}
+			}
+			return enPrimeStatus::Prime;
+		}
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	int Num;
+	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+	enPrimeStatus  PrimeStatus = CheckPrimeNumber(Num);
+	PrintPrimeNumber(PrimeStatus, Num);
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
--- algorithms_level1/50.cpp
+++ algorithms_level1/50.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-using namespace std;
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return Num;
-}
-void CheckATMPassword3Times() {
-	int ATMPassword = 0;
-	int counter = 1;
-	while (true) {
-		if (counter <= 3) {
-			ATMPassword = ReadPositiveNumber("ENTER ATM Password: ");
-			if (ATMPassword == 1234) {
-				system("color 2F");
-				cout << '\a' << "Your Balance is: 7500" << endl;
-				break;
-			}
-			else {
-				system("color 6F");
-				cout << '\a' << "Wong PIN" << endl;
-			}
-			counter++;
-		}
-		else {
-			system("color 4F");
-			cout << '\a' << "Card is locked!" << endl;
-			break;
-		}
-
-
-	}
-}
-
-
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	CheckATMPassword3Times();
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
-
+//#include <iostream>
+//using namespace std;
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return Num;
+//}
+//void CheckATMPassword3Times() {
+//	int ATMPassword = 0;
+//	int counter = 1;
+//	while (true) {
+//		if (counter <= 3) {
+//			ATMPassword = ReadPositiveNumber("ENTER ATM Password: ");
+//			if (ATMPassword == 1234) {
+//				system("color 2F");
+//				cout << '\a' << "Your Balance is: 7500" << endl;
+//				break;
+//			}
+//			else {
+//				system("color 6F");
+//				cout << '\a' << "Wong PIN" << endl;
+//			}
+//			counter++;
+//		}
+//		else {
+//			system("color 4F");
+//			cout << '\a' << "Card is locked!" << endl;
+//			break;
+//		}
+//
+//
+//	}
+//}
+//
+//
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	CheckATMPassword3Times();
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
+//
