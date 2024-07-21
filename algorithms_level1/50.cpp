973f8b8dcf1bb7de1fa0e71ccd02aa0435b0bed8
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 10:49:08 +0300

    CheckATMPassword

---
+++ algorithms_level1/50.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return Num;
+}
+void CheckATMPassword() {
+	int ATMPassword = 0;
+	int counter = 1;
+	while (true) {
+		ATMPassword = ReadPositiveNumber("ENTER ATM Password: ");
+		if (ATMPassword == 1234) {
+			system("color 2F");
+			cout << '\a' << "Your Balance is: 7500" << endl;
+			break;
+		}
+		else {
+			system("color 6F");
+			cout << '\a' << "Wong PIN" << endl;
+
+		}
+		counter++;
+
+
+
+	}
+
+}
+
+
+
+//if (ATMPassword == 1234) {
+//	system("color 2F");
+//	counter++;
+//}
+//else {
+//	system("color 4F");
+//	cout << '\a' << "Your Balance is: 7500" << endl;
+//	break;
+//};
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	CheckATMPassword();
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
+
