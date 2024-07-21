dce301ad6001306a722b1d94e2862e4e410c5827
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 07:55:55 +0300

    ServicesFeeAndSalesTax

--- algorithms_level1/39.cpp
+++ algorithms_level1/39.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-using namespace std;
-
-double  ReadPositiveNumber(string Message) {
-	double Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return Num;
-}
-void PrintResult(double Remainder) {
-	cout << "---------------------------\n";
-	cout << "Remainder: " << Remainder << endl;
-}
-
-double CalculateRemainder(double TotalBill, double CashPaid) {
-	return CashPaid - TotalBill;
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	double TotalBill, CashPaid;
-	TotalBill = ReadPositiveNumber("Enter TotalBill: ");
-	CashPaid = ReadPositiveNumber("Enter CashPaid: ");
-	PrintResult(CalculateRemainder(TotalBill, CashPaid));
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//double  ReadPositiveNumber(string Message) {
+//	double Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return Num;
+//}
+//void PrintResult(double Remainder) {
+//	cout << "---------------------------\n";
+//	cout << "Remainder: " << Remainder << endl;
+//}
+//
+//double CalculateRemainder(double TotalBill, double CashPaid) {
+//	return CashPaid - TotalBill;
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	double TotalBill, CashPaid;
+//	TotalBill = ReadPositiveNumber("Enter TotalBill: ");
+//	CashPaid = ReadPositiveNumber("Enter CashPaid: ");
+//	PrintResult(CalculateRemainder(TotalBill, CashPaid));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
