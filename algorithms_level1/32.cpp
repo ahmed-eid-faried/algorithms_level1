d4e059d1a3feb3d4a87810184781963eeee60c09
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-23 13:03:51 +0300

    finish

--- algorithms_level1/32.cpp
+++ algorithms_level1/32.cpp
@@ -1,34 +1,34 @@
-//#include <iostream>
-//using namespace std;
-//int  ReadPositiveNumber(string Message) {
-//	int Num;
-//	do {
-//		cout << Message;
-//		cin >> Num;
-//	} while (Num < 0);
-//	return Num;
-//}
-//void PrintResult(int Num, string title) {
-//	cout << title << "====>>> " << Num << endl;
-//}
-//
-//void PrintCalculatePowerOfNumber(int Num, int M) {
-//	int PowerOfNumber = 1;
-//	for (int i = 1; i <= M; i++) {
-//		PowerOfNumber *= Num;
-//	}
-//	PrintResult(PowerOfNumber, "Number^M :");
-//
-//}
-//
-//int main() {
-//	cout << "########################################################\n";
-//	cout << "#####################-by-ahmed-mady-####################\n";
-//	cout << "########################################################\n";
-//	int Num = ReadPositiveNumber("ENTER Num: ");
-//	int M = ReadPositiveNumber("ENTER M: ");
-//	PrintCalculatePowerOfNumber(Num, M);
-//	cout << "########################################################\n";
-//	cout << "########################################################\n";
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+float  ReadPositiveNumber(string Message) {
+	float Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return Num;
+}
+void PrintResult(float Num, string title) {
+	cout << title << "====>>> " << Num << endl;
+}
+
+void PrintCalculatePowerOfNumber(float Num, float M) {
+	float PowerOfNumber = 1;
+	for (float i = 1; i <= M; i++) {
+		PowerOfNumber *= Num;
+	}
+	PrintResult(PowerOfNumber, "Number^M :");
+
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	float Num = ReadPositiveNumber("ENTER Num: ");
+	float M = ReadPositiveNumber("ENTER M: ");
+	PrintCalculatePowerOfNumber(Num, M);
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
--- algorithms_level1/38print_prime_number.cpp
+++ algorithms_level1/38print_prime_number.cpp
@@ -1,59 +1,59 @@
-#include <iostream>
-using namespace std;
-enum enPrimeOrNotPrime { Prime, NotPrime };
-
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return Num;
-}
-void PrintPrimeNumber(enPrimeOrNotPrime  PrimeStatus, int Num) {
-	cout << "---------------------------\n";
-	if (PrimeStatus == enPrimeOrNotPrime::Prime) {
-		cout << "Prime: " << Num << endl;
-	}
-	else {
-		cout << "Not Prime: " << Num << endl;
-	}
-}
-bool CheckEvenNumber(int Num) {
-	return Num % 2 == 0 || Num == 0;
-}
-bool CheckNumberIs1Or2(int Num) {
-	return(Num == 1 || Num == 2);
-}
-enPrimeOrNotPrime CheckPrimeNumber(int Num) {
-	if (CheckNumberIs1Or2(Num)) {
-		return enPrimeOrNotPrime::Prime;
-
-	}
-	else
-		if (CheckEvenNumber(Num)) {
-			return enPrimeOrNotPrime::NotPrime;
-		}
-		else
-		{
-			for (int i = 2; i < ceil(Num / 2); i++) {
-				if (Num % i == 0) {
-					return enPrimeOrNotPrime::NotPrime;
-				}
-			}
-			return enPrimeOrNotPrime::Prime;
-		}
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	int Num;
-	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
-	enPrimeOrNotPrime  PrimeStatus = CheckPrimeNumber(Num);
-	PrintPrimeNumber(PrimeStatus, Num);
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//enum enPrimeOrNotPrime { Prime, NotPrime };
+//
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return Num;
+//}
+//void PrintPrimeNumber(enPrimeOrNotPrime  PrimeStatus, int Num) {
+//	cout << "---------------------------\n";
+//	if (PrimeStatus == enPrimeOrNotPrime::Prime) {
+//		cout << "Prime: " << Num << endl;
+//	}
+//	else {
+//		cout << "Not Prime: " << Num << endl;
+//	}
+//}
+//bool CheckEvenNumber(int Num) {
+//	return Num % 2 == 0 || Num == 0;
+//}
+//bool CheckNumberIs1Or2(int Num) {
+//	return(Num == 1 || Num == 2);
+//}
+//enPrimeOrNotPrime CheckPrimeNumber(int Num) {
+//	if (CheckNumberIs1Or2(Num)) {
+//		return enPrimeOrNotPrime::Prime;
+//
+//	}
+//	else
+//		if (CheckEvenNumber(Num)) {
+//			return enPrimeOrNotPrime::NotPrime;
+//		}
+//		else
+//		{
+//			for (int i = 2; i < ceil(Num / 2); i++) {
+//				if (Num % i == 0) {
+//					return enPrimeOrNotPrime::NotPrime;
+//				}
+//			}
+//			return enPrimeOrNotPrime::Prime;
+//		}
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	int Num;
+//	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+//	enPrimeOrNotPrime  PrimeStatus = CheckPrimeNumber(Num);
+//	PrintPrimeNumber(PrimeStatus, Num);
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
